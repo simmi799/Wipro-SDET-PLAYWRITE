076990cd087ef3410945689529b0318829bc0efc
simmi799 <ssimmisharma553@gmail.com>
2026-04-30 12:27:03 +0530

    updated code

--- Day1/index.js
+++ Day1/index.js
@@ -1,3 +1,26 @@
 let a = 5;
-let b= 10;
-console.log(a + b);
+let b = 10;
+
+let str = "Hello, World!";
+
+// String operations
+console.log("String:", str);
+console.log("Length of string:", str.length);
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
+console.log("Substring (0-5):", str.substring(0, 5)); // 0 - first character, 5 - up to but not including the 5th character
+
+// Mathematical operations
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b); // Remainder (Modulo)
+
+// Logical operations --> bool
+console.log("a > b:", a > b); // F
+console.log("a < b:", a < b); // T
+console.log("a == b:", a == b); // F === Strict equality (checks value and type)
+console.log("a != b:", a != b); // T
+console.log("a >= b:", a >= b); // F
+console.log("a <= b:", a <= b); // T
