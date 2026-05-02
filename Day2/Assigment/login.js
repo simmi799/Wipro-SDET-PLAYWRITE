bf26356b8a7bedecd8aa1c34b522913b14b5e52d
simmi799 <ssimmisharma553@gmail.com>
2026-05-02 17:00:20 +0530

    Question

--- Day2/Assigment/login.js
+++ Day2/Assigment/login.js
@@ -2,6 +2,11 @@ const login = () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+     if (username === "" || password === "") {
+        alert("Please fill all fields!");
+        return;
+    }
+    
     if (username === "simmi" && password === "simmi799") {
         alert("Login successful!");
         window.location.href = "app.html";
