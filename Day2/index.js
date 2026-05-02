83eea0e0f116f8b7a8d1d905a4c186f3583dd252
simmi799 <ssimmisharma553@gmail.com>
2026-05-02 11:26:05 +0530

    updated

--- Day2/index.js
+++ Day2/index.js
@@ -26,5 +26,27 @@ console.log(table(20));
 // console.log(sum(5,10));
 
 
-let arr = [1,2,3,4,5,6, "Simmi"];
-arr.filter(x=> typeof x==="number").map(y =>y*2).forEach(z=> console.log("Elements *2:" , z*2));
+// let arr = [1,2,3,4,5,6, "Simmi"];
+// arr.filter(x=> typeof x==="number").map(y =>y*2).forEach(z=> console.log("Elements *2:" , z*2));
+
+
+console.log(Math.random());
+console.log(Math.random() * 100);
+console.log(Math.ceil(4.2)); 
+console.log(Math.ceil(7.8)); 
+console.log(Math.ceil(5)); 
+console.log(Math.floor(Math.random() * 100));  
+
+console.log(parseInt("45.67"));  
+console.log(parseInt("100px"));  
+console.log(parseInt("abc"));    
+
+let color= ["green", "red", "blue", "yellow"];
+
+console.log(color.push("orange"));
+console.log(color.pop());
+console.log(color.shift());
+console.log(color.unshift("purple"));
+// console.log(color.splice(1, 0, "pink"));
+// console.log(color.splice(1, 3));
+console.log(color.indexOf("blue"));
