efcf4f66b9e651ec1c500457409afd739d50a0f0
simmi799 <ssimmisharma553@gmail.com>
2026-05-05 12:34:28 +0530

    JSON

--- Day4/index.js
+++ Day4/index.js
@@ -1,3 +1,32 @@
+// function getData(){
+//     return new Promise((resolve , reject) => {
+//         setTimeout(() => {
+//             resolve("Data received");
+//         },5000);
+//     });
+// }
+// getData().then((data)=>{
+//     console.log(data);
+// }).catch((error)=>{
+//     console.error("Error :", error);
+// })
+// setTimeout(()=>{
+//     console.log ("new data");
+// },5000)
+
+// function getData(){
+//     return new Promise((resolve , reject) => {
+//         setTimeout(() => {
+//             reject("Data received");
+//         },5000);
+//     });
+// }
+// getData().then((data)=>{
+//     console.log(data);
+// }).catch((error)=>{
+//     console.error("Error :", error);
+// })
+
 function getData(){
     return new Promise((resolve , reject) => {
         setTimeout(() => {
@@ -5,24 +34,10 @@ function getData(){
         },5000);
     });
 }
-getData().then((data)=>{
-    console.log(data);
-}).catch((error)=>{
-    console.error("Error :", error);
-})
-setTimeout(()=>{
-    console.log ("new data");
-},5000)
 
-function getData(){
-    return new Promise((resolve , reject) => {
-        setTimeout(() => {
-            reject("Data received");
-        },5000);
-    });
-}
-getData().then((data)=>{
-    console.log(data);
-}).catch((error)=>{
-    console.error("Error :", error);
-})
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Aaryan " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
