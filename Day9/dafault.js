4436b46e25fb6ed452376eb1742a94251dc6ca63
simmi799 <ssimmisharma553@gmail.com>
2026-05-11 11:59:18 +0530

    Api

--- Day9/dafault.js
+++ Day9/dafault.js
@@ -14,3 +14,29 @@ console.log(newNums);
 const obj = { name : " Shiv" , age : 52};
 const updatedObj= {...obj ,name :" Shankara", age: 25};
 console.log(updatedObj);
+
+const cal = { // rest operator
+    sum :(...nums) => nums.reduce((a,b)=> a+b , 0), // rest parameters
+
+}
+
+console.log("Rest operator");
+console.log(cal.sum(1,5,36,78));
+
+
+
+// destructuring
+// const arr = ["Arun", "Bhavi", "Chandru", "Dhanush"];
+// const first = arr[0];
+// const second = arr[1];
+// const third = arr[2];
+// const fourth = arr[3];
+
+const [first, second, third, fourth, fifth] = ["Simmi", "Summi", "Sharma", "Kumari"]; // pos-based extraction
+const [one, two, ...three] = [1, 3, 5, 6, 7];
+console.log("\n4. Destructuring:");
+console.log(first, second, third, fourth, fifth);
+console.log(one, two, three)
+
+const { name, age } = {name: "shiva", age: 22}; // key-based extracted
+console.log(name, age);
